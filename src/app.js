f0de9617e156856bbd5df175cde83d079d24f73d
Jai <jaideep.s@hackerearth.com>
2018-10-30 01:20:59 +0530

    Separate CSS for production and CSS source Map

--- src/app.js
+++ src/app.js
@@ -3,12 +3,11 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
-import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 const store = configureStore();
+console.log("Just copying and pasting, no I am typing it");
 const jsx = (
     <Provider store={store}>
         <AppRouter />
